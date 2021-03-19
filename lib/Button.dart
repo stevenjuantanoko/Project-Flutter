775b935fd876439afd63a23b490933eb400b1d4c
Stevjuan <stevjuan000@gmail.com>
2021-03-19 11:34:01 +0700

    updaed

--- lib/Button.dart
+++ lib/Button.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'mainPage.dart';
+
 class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      margin: EdgeInsets.symmetric(horizontal: 50),
-      decoration: BoxDecoration(
-        color: Colors.cyan[500],
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Center(
-        child: Text(
-          "Login",
-          style: TextStyle(
-              color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+    return Scaffold(
+        body: Container(
+            height: 50,
+            margin: EdgeInsets.symmetric(horizontal: 50),
+            decoration: BoxDecoration(
+              color: Colors.cyan[500],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return MainPage();
+              }));
+            }));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'Login.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: Scaffold(),
     );
   }
 }
