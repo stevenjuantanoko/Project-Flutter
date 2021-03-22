4029e064caa4e9b653ca3f4d0854a6c4a592b027
Stevjuan <stevjuan000@gmail.com>
2021-03-22 12:40:00 +0700

    updated

--- lib/Button.dart
+++ lib/Button.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'mainPage.dart';
+import 'MainPage.dart';
 
 class Button extends StatelessWidget {
+  void DataPass(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MainPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            height: 50,
-            margin: EdgeInsets.symmetric(horizontal: 50),
-            decoration: BoxDecoration(
-              color: Colors.cyan[500],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Center(
-              child: Text(
-                "Login",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) {
-                return MainPage();
-              }));
-            }));
+        body: Center(
+      child: RaisedButton(
+        child: Text(
+          'Login',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {
+          DataPass(context);
+        },
+      ),
+    ));
   }
 }
--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,36 +1,102 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'Header.dart';
-import 'Wrapper.dart';
+import 'InputWrapper.dart';
+import 'package:http/http.dart' as http;
 
-class MainPage extends StatelessWidget {
+mixin MainPage implements StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List users = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  fetchUsers() async {
+    var url = Uri.parse("https://reqres.in/api/users");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var items = json.decode(response.body)['data'];
+      setState(() {
+        users = items;
+      });
+    } else {
+      setState(() {
+        users = [];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              colors: [Colors.cyan[500], Colors.cyan[300], Colors.cyan[400]]),
-        ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            Header(),
-            Expanded(
-                child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(60),
-                    topRight: Radius.circular(60),
-                  )),
-              child: InputWrapper(),
-            ))
-          ],
+      appBar: AppBar(
+        title: Text("List Users"),
+      ),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    List items = ["1", "2"];
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return getCard(index);
+        });
+  }
+
+  Widget getCard(index) {
+    var name = index['first_name'] + " " + index['last_name'];
+    var email = index['email'];
+    var image = index['avatar'];
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListTile(
+          title: Row(
+            children: <Widget>[
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(60 / 2),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(image.toString()))),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    name.toString(),
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    email.toString(),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
